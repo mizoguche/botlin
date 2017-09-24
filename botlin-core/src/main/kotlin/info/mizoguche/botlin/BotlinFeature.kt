6756d8b7a57d02c501023f23ac1e07c1dc8e41bd
Michiaki Mizoguchi <michiaki.mizoguchi@gmail.com>
2017-09-25 05:59:45 +0900

    Fix argument name

--- botlin-core/src/main/kotlin/info/mizoguche/botlin/BotlinFeature.kt
+++ botlin-core/src/main/kotlin/info/mizoguche/botlin/BotlinFeature.kt
@@ -5,7 +5,7 @@ interface BotlinFeature {
 }
 
 interface BotlinFeatureFactory<out C : Any, out F : BotlinFeature> {
-    fun create(configuration: C.() -> Unit): F
+    fun create(configure: C.() -> Unit): F
 }
 
 interface BotlinSubscriber<in T> {
