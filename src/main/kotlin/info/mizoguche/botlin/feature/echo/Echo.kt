6f6049d6856c28eed9b54b068924b571d42c3e6c
Michiaki Mizoguchi <michiaki.mizoguchi@gmail.com>
2017-10-01 22:18:39 +0900

    Add Echo

---
+++ src/main/kotlin/info/mizoguche/botlin/feature/echo/Echo.kt
@@ -0,0 +1,30 @@
+package info.mizoguche.botlin.feature.echo
+
+import info.mizoguche.botlin.BotlinFeatureFactory
+import info.mizoguche.botlin.BotlinFeatureId
+import info.mizoguche.botlin.feature.command.BotlinCommand
+import info.mizoguche.botlin.feature.command.CommandFeature
+
+class Echo : CommandFeature() {
+    override val command: String
+        get() = "echo"
+    override val description: String
+        get() = "echo"
+    override val usage: String
+        get() = "echo hello"
+
+    override fun onCommandPublishing(command: BotlinCommand) {
+        command.msgEvent.reply(command.args)
+    }
+
+    override val id: BotlinFeatureId
+        get() = BotlinFeatureId("Echo")
+
+    class Configuration
+
+    companion object Factory : BotlinFeatureFactory<Configuration, Echo> {
+        override fun create(configure: Configuration.() -> Unit): Echo {
+            return Echo()
+        }
+    }
+}
