3605f475e8544367f99f5fd809c260051a1b84c0
Michiaki Mizoguchi <michiaki.mizoguchi@gmail.com>
2017-11-09 18:21:31 +0900

    Add bot message interfaces

--- src/main/kotlin/info/mizoguche/botlin/BotMessage.kt
+++ src/main/kotlin/info/mizoguche/botlin/BotMessage.kt
@@ -1,3 +1,32 @@
 package info.mizoguche.botlin
 
-interface BotMessage
+interface BotMessage {
+    val channelId: String
+
+    /**
+     * Received message omitted mentionPrefix
+     */
+    val message: String
+    val rawMessage: String
+    val sender: BotMessageSender
+    val session: BotMessageSession
+    val isMention: Boolean
+        get() = message != rawMessage
+
+    fun reply(body: String)
+}
+
+interface BotMessageSender {
+    val senderId: String
+    val senderUserName: String
+    val senderDisplayName: String
+}
+
+interface BotMessageSession {
+    val mentionChar: String
+    val userName: String
+    val mentionPrefix: String
+        get() = "$mentionChar$userName"
+}
+
+data class BotMessageRequest(val channelId: String, val message: String)
